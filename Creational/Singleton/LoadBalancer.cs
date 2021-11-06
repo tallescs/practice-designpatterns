ec72826fc8940631bcefd011c551d7d02ccc3a81
Talles <tallescs@gmail.com>
2021-11-06 12:22:28 -0300

    Creational Folder. Singleton pattern demo created using explicit lock and possible .net optimization commented. This singleton example is a abstraction of a Load Balancer that randomly returns a Server.

---
+++ Creational/Singleton/LoadBalancer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton
+{
+    public sealed class LoadBalancer
+    {
+        // Optimization that would remove the use of lock(_locker):
+        // this optimization is not used for study/demo purpose.
+        // Static members are initialized immediately when class is loaded for the first time.
+        // .NET guarantees thread safety for static initialization
+        // private static readonly LoadBalancer instance = new LoadBalancer();
+
+        private static LoadBalancer _instance;
+        private static readonly object _locker = new();
+        private readonly IList<Server> servers;
+        private readonly Random random = new();
+
+        private LoadBalancer()
+        {
+            servers = new List<Server>
+                {
+                  new Server{ Name = "ServerI", IP = "120.14.220.18" },
+                  new Server{ Name = "ServerII", IP = "120.14.220.19" },
+                  new Server{ Name = "ServerIII", IP = "120.14.220.20" },
+                  new Server{ Name = "ServerIV", IP = "120.14.220.21" },
+                  new Server{ Name = "ServerV", IP = "120.14.220.22" },
+                };
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+            //double checked locking pattern
+            if (_instance == null)
+            {
+                lock (_locker)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public Server NextServer
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r];
+            }
+        }
+    }
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CheckBalancers();
+           
+            var balancer = LoadBalancer.GetLoadBalancer();
+            
+            for (int i = 0; i < 20; i++)
+            {
+                var serverName = balancer.NextServer.Name;
+                Console.WriteLine($"Request dispatched to: {serverName}");
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void CheckBalancers()
+        {
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+            var b4 = LoadBalancer.GetLoadBalancer();
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Balancers are same instance.");
+            }
+        }
+    }
+}
